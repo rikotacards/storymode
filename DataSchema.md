fa44dada30b175bb04672b8d6f375476d5fef809
rikotacards <maxhsu1@gmail.com>
2023-03-21 16:08:36 +0800

    add firebase, add documentation for data struture, and added helpful resources.

--- DataSchema.md
+++ DataSchema.md
@@ -37,4 +37,21 @@
 `/[env]/[username]/[postId]/[fileName]`
 
 # Post db path
-`/`
+
+## Option 1
+
+`/content/[userid]/posts/[postId]/content`
+- This path gets us all images / text content for a specific post
+- We would use this path to render a post
+
+* `/content` lowest level
+* `/content/[userId]` so we can get all posts from that user, or get all posts from followers
+* `/content/[userId]/posts` to get all posts for that userId
+* `/content/[userId]/posts/[postId]`, access specific post
+* `/content/[userid]/posts/[postId]/content` access list of images or text post
+
+## Option 2
+``/content/[userid]/posts/[postId]/`
+- Shorter, and we nest all information into postId. The post document would include the list of images / text
+- Reactions would be a key, mapped to a map `reactions: {happy: 0, sad: 1}`
+
---
+++ src/firebase/clientApp.ts
@@ -0,0 +1,35 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getFirestore} from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCihlRapz-_cQunIxs8f8Rxs7fLj0wyjUA",
+  authDomain: "storymode-app.firebaseapp.com",
+  projectId: "storymode-app",
+  storageBucket: "storymode-app.appspot.com",
+  messagingSenderId: "546787786385",
+  appId: "1:546787786385:web:4401705f292b9dd0ca4930",
+  measurementId: "G-4Y0H8V2VZY"
+};
+
+// Initialize Firebase
+const app = initializeApp({
+  apiKey:process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain:process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId:process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket:process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId:process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId:process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+});
+console.log('hi',process.env)
+// const analytics = getAnalytics(app);
+const firestore = getFirestore();
+
+export {firestore};
