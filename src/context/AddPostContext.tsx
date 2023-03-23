70d8dfe0496460779b2aa7fcd8389643c9162ebc
rikotacards <maxhsu1@gmail.com>
2023-03-23 15:46:12 +0800

    add new page, add ability to get my post data.

--- src/context/AddPostContext.tsx
+++ src/context/AddPostContext.tsx
@@ -2,11 +2,20 @@ import React from 'react';
 import {  collection, doc, setDoc, Timestamp } from "firebase/firestore";
 import { firestore } from "@/firebase/clientApp";
 import { getStorage, ref, uploadString } from "firebase/storage";
+import { uploadPost } from '@/firebase/db';
 
 const storage = getStorage();
 
+export interface Content {
+  imageUrl: string; 
+  caption: string;
+  blobData: string;
+}
+
+
+
 interface AddPostContextProps {
-  posts: {imageUrl: string, caption: string, blobData: string}[];
+  posts: {author: string, content: Content[], postTime: Date};
   addPost: () => void;
   onTextChange: (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>, i: number) => void;
   addImage: (imageUrl: string, index: number, blobData:string) => void;
@@ -20,7 +29,7 @@ export const AddPostContext = React.createContext({} as AddPostContextProps)
 interface PostContextProps {
   children: React.ReactNode;
 }
-const username = 'max'
+export const username = 'max'
 export const AddPostContextWrapper: React.FC<PostContextProps> = ({children}) => {
   const [posts, setPosts] = React.useState([{imageUrl:'', caption:'', blobData: ''}])
   const collectionRef = collection(firestore,'content', username, 'posts')
@@ -28,29 +37,14 @@ export const AddPostContextWrapper: React.FC<PostContextProps> = ({children}) =>
 
   
   const onPostClick = async() => {
-    try{
-
-      await setDoc(doc(firestore, "content", username, "posts", docRef.id), {
-        postTime: Timestamp.fromDate(new Date()),
-        author: username,
-        content: posts
-      });
-
-      posts.forEach(async(post,i) => {
-        // we save images into a directory that references the post
-        const storageRef = ref(storage, `${username}/${docRef.id}/${i}.jpg`,);
-        if(!post.blobData){
-          return 
-        }
-        await uploadString(storageRef, post.blobData, 'data_url').then((snapshot) => {
-          console.log('Uploaded a blob or file!');
-        });
-      })
-      
-    }catch (e){
-      console.log("ER", e)
-    }
-  
+   try {
+    const res = uploadPost({
+      username, 
+      posts
+    })
+   } catch (e) {
+    console.log(e)
+   }
   }
   
 
