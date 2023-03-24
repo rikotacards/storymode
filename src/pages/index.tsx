d1e03eaf396ee4fb63121e212691fc15435762b5
rikotacards <maxhsu1@gmail.com>
2023-03-24 16:16:02 +0800

    remove unused components from index.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,8 +17,7 @@ export default function Home() {
         
       </Head>
       <main className={styles.main}>
-        <TextOnlyPost/>
-        <PostWithImage/>
+      
       </main>
     </>
   )
