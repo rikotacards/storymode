c7ef7e157059b84233023592f6f3e86634123fe3
rikotacards <maxhsu1@gmail.com>
2023-03-27 10:27:00 +0800

    remove unused.

--- src/components/TabPanel/TabPanel.tsx
+++ src/components/TabPanel/TabPanel.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import SwipeableViews from 'react-swipeable-views';
 import { useTheme } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Tabs from '@mui/material/Tabs';
