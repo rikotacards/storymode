e4b54aa6b4c8187be4e267a49f25c191a3ea0792
rikotacards <maxhsu1@gmail.com>
2023-03-29 17:48:20 +0800

    sidem.

--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -1,80 +1,102 @@
-import { Avatar, Button, Card, CSSObject, MenuList, Paper, styled, SwipeableDrawer, Theme } from "@mui/material";
+import {
+  Avatar,
+  Button,
+  Card,
+  CSSObject,
+  MenuList,
+  Paper,
+  styled,
+  SwipeableDrawer,
+  Theme,
+} from "@mui/material";
 import React from "react";
-import MuiDrawer from '@mui/material/Drawer';
+import MuiDrawer from "@mui/material/Drawer";
 
 import { MenuItem } from "../MenuItem/MenuItem";
 import styles from "./SideMenu.module.css";
-import HomeIcon from '@mui/icons-material/Home';
-import ExploreIcon from '@mui/icons-material/Explore';
-import AddIcon from '@mui/icons-material/Add';
+import HomeIcon from "@mui/icons-material/Home";
+import ExploreIcon from "@mui/icons-material/Explore";
+import AddIcon from "@mui/icons-material/Add";
 import { useAuth } from "@/context/AuthContext";
 export const sideMenuWidth = 240;
-import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from "@mui/icons-material/Menu";
 import { useGetUserInfo } from "@/hooks/useGetUserInfo";
-export  const menuItems = [
-  { name: "Home", path: "/", icon: <HomeIcon/> },
-  { name: "Explore", path: "/explore", icon: <ExploreIcon/> },
-  { name: "Add Post", path: "/add-post", icon: <AddIcon/> },
-
+export const menuItems = [
+  { name: "Home", path: "/", icon: <HomeIcon /> },
+  { name: "Explore", path: "/explore", icon: <ExploreIcon /> },
+  { name: "Add Post", path: "/add-post", icon: <AddIcon /> },
 ];
 const openedMixin = (theme: Theme): CSSObject => ({
   width: sideMenuWidth,
-  transition: theme.transitions.create('width', {
+  transition: theme.transitions.create("width", {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.enteringScreen,
   }),
-  overflowX: 'hidden',
+  overflowX: "hidden",
 });
 
 const closedMixin = (theme: Theme): CSSObject => ({
-  transition: theme.transitions.create('width', {
+  transition: theme.transitions.create("width", {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
-  overflowX: 'hidden',
+  overflowX: "hidden",
   width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up('sm')]: {
+  [theme.breakpoints.up("sm")]: {
     width: `calc(${theme.spacing(8)} + 1px)`,
   },
 });
 
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme, open }) => ({
-    width: sideMenuWidth,
-    flexShrink: 0,
-    whiteSpace: 'nowrap',
-    boxSizing: 'border-box',
-    ...(open && {
-      ...openedMixin(theme),
-      '& .MuiDrawer-paper': openedMixin(theme),
-    }),
-    ...(!open && {
-      ...closedMixin(theme),
-      '& .MuiDrawer-paper': closedMixin(theme),
-    }),
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  width: sideMenuWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  ...(open && {
+    ...openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme),
   }),
-);
+  ...(!open && {
+    ...closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
+  }),
+}));
 
 export const SideMenu: React.FC = () => {
-  
-  const auth = useAuth()
-  const userInfo = useGetUserInfo(auth?.uid as string)
-  console.log(auth.isLoggedIn)
-  if(!auth.isLoggedIn){
-    return null
+  const auth = useAuth();
+  const userInfo = useGetUserInfo(auth?.uid as string);
+  if (!auth.isLoggedIn) {
+    return null;
   }
-  const all = [...menuItems,   { name: "Profile", path: "/"+ (userInfo.username || 'signin'), icon: <Avatar sx={{height: 24, width: 24}}/> },
-]
-  
+  const all = [
+    ...menuItems,
+    {
+      name: "Profile",
+      path: "/" + (userInfo.username || "signin"),
+      icon: <Avatar sx={{ height: 24, width: 24 }} />,
+    },
+  ];
 
   return (
     <Paper>
       <Drawer anchor={"left"} open={true} variant="permanent">
         {all.map((item) => (
-          <MenuItem icon={item.icon} key={item.name} name={item.name} path={item.path} />
+          <MenuItem
+            icon={item.icon}
+            key={item.name}
+            name={item.name}
+            path={item.path}
+          />
         ))}
         <div>
-          <MenuItem name={'sign out'} icon={<MenuIcon/>} path='/signout' onClick={auth.signOut}/>
+          <MenuItem
+            name={"sign out"}
+            icon={<MenuIcon />}
+            path="/signout"
+            onClick={auth.signOut}
+          />
         </div>
       </Drawer>
     </Paper>
