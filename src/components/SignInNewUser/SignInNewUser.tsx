70894c166795f093b5a0edea0c694490fb253238
rikotacards <maxhsu1@gmail.com>
2023-06-08 15:13:29 +0800

    wip

--- src/components/SignInNewUser/SignInNewUser.tsx
+++ src/components/SignInNewUser/SignInNewUser.tsx
@@ -1,51 +1,43 @@
-import React from 'react'
-import {  createUserWithEmailAndPassword, getAuth, getRedirectResult, GoogleAuthProvider, signInWithPopup, signInWithRedirect, User } from "firebase/auth";
-import { Button, Card, CardContent, Input } from '@mui/material';
-import { AuthContext } from '@/context/AuthContext';
-import { auth } from '@/firebase/clientApp';
-import { useRouter } from 'next/router';
-import { isProtectedRoute } from '@/constants/routes';
+import React from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  getRedirectResult,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signInWithRedirect,
+  User,
+} from "firebase/auth";
+import { Button, Card, CardContent, Input } from "@mui/material";
+import { AuthContext } from "@/context/AuthContext";
+import { auth } from "@/firebase/clientApp";
+import { useRouter } from "next/router";
+import { isProtectedRoute } from "@/constants/routes";
 
 export const SignInNewUser: React.FC = () => {
   const route = useRouter();
-  const auth = getAuth()
+  const auth = getAuth();
   const provider = new GoogleAuthProvider();
 
-  
-
-  // getRedirectResult(auth)
-  // .then((result) => {
-  //   console.log('omgwhat', result)
-  //   // This gives you a Google Access Token. You can use it to access Google APIs.
-  //   const credential = result && GoogleAuthProvider.credentialFromResult(result);
-  //   const token = credential?.accessToken;
-
-  //   // The signed-in user info.
-  //   const user = result?.user;
-  //   // IdP data available using getAdditionalUserInfo(result)
-  //   // ...
-  //   route.push('/')
-  // }).catch((error) => {
-  //   // Handle Errors here.
-  //   const errorCode = error.code;
-  //   const errorMessage = error.message;
-  //   // The email of the user's account used.
-  //   const email = error.customData.email;
-  //   // The AuthCredential type that was used.
-  //   const credential = GoogleAuthProvider.credentialFromError(error);
-  //   // ...
-  // });
-
-  
 
   return (
-    <div style={{display: 'flex', width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center'}}>
-   
-      <Button variant='contained'  onClick={(e) => {  signInWithRedirect(auth,provider )
-}}>
+    <div
+      style={{
+        display: "flex",
+        width: "100%",
+        height: "100%",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Button
+        variant="contained"
+        onClick={(e) => {
+          signInWithRedirect(auth, provider);
+        }}
+      >
         Sign In with Google
       </Button>
-    
     </div>
-  )
-}
+  );
+};
--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -5,24 +5,38 @@ import { useAuth } from "@/context/AuthContext";
 import { useRouter } from "next/router";
 import { getAuth, getRedirectResult } from "firebase/auth";
 import { LinearProgressCustom } from "@/components/LinearProgressCustom/LinearProgressCustom";
-import { Toolbar } from "@mui/material";
+import { Button, Toolbar, Typography } from "@mui/material";
 
 const SignIn = () => {
   const auth = useAuth();
   const [isLoading, setLoading] = React.useState(true);
+  const [username, setUsername] = React.useState('')
   const auth2 = getAuth();
   const router = useRouter();
   React.useEffect(() => {
+    setUsername('loading')
     console.log("SignIn running");
+    if(auth.isLoggedIn){
+      console.log('aready')
+      setUsername('already')
+      router.push('/')
+    }
     getRedirectResult(auth2)
+    
       .then((user) => {
+        if(user === null){
+          setUsername('NULL')
+        }
         console.log("getting redirect result", user);
         console.log("pushing");
         if (user?.user) {
+          console.log(user)
+          setUsername(user?.user.displayName || 'yay')
           router.push("/");
           return;
         } else {
           setLoading(false)
+          // setUsername('whatever')
         }
       })
       
@@ -40,10 +54,13 @@ const SignIn = () => {
           justifyContent: 'center',
           width: "100%",
           height: "100vh",
-          border: '1px solid white'
         }}
       >
         <Toolbar/>
+  
+        <Button>
+          {username}
+          </Button>
         <SignInNewUser />
       </div>
     </>
